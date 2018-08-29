90aa864e265263c1c9253de460ac53f5d1b8c622
alex-phillips <ahp118@gmail.com>
2018-08-29 15:19:19 -0400

    moved stuff around, fixed setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
-'''
+"""
 Installer script for ripper.
-'''
+"""
 
 from setuptools import setup
 
-setup (
-    name = "ripper",
-    description = "Automatically transcode and upload FLACs on What.CD.",
-    author = 'Zach Denton',
-    author_email = 'zacharydenton@gmail.com',
-    version = '1.0',
-    url = 'http://zacharydenton.com/code/whatbetter/',
-    py_modules = ['ripper'],
-    scripts = ['ripper'],
-    install_requires = [
-                'argparse',
-                'BeautifulSoup',
-                ]
+setup(
+    name="piwigo",
+    description="CLI and SDK for Piwigo",
+    author="Alex Phillips",
+    author_email="ahp118@gmail.com",
+    version="1.0",
+    url="",
+    py_modules=["piwigo"],
+    entry_points = {
+        'console_scripts': [
+            'piwigo = piwigo.cli:main'
+        ]
+    },
+    install_requires=["requests"],
 )
